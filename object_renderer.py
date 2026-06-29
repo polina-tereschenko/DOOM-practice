59d4c9ac6229ec9fb793b661cfd82838cb32a744
Polina Tereschenko <polinatereschenko16@gmail.com>
2026-06-29 23:59:51 +0300

    add sprites

--- object_renderer.py
+++ object_renderer.py
@@ -2,7 +2,7 @@ import pygame as pg
 from settings import *
 
 class ObjectRenderer:
-    def __init__(self, game):
+    def __init__ (self, game):
         self.game = game
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
@@ -21,7 +21,7 @@ class ObjectRenderer:
         pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
         
     def render_game_objects(self):
-        list_objects = self.game.raycasting.objects_to_render
+        list_objects = sorted(self.game.raycasting.objects_to_render, key=lambda t: t[0], reverse=True)
         for depth, image, pos in list_objects:
             self.screen.blit(image, pos)
 
